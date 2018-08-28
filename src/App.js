748650718cd73d6b7d2afecc629196aef802881a
Aswin A V <aswinavofficial@gmail.com>
2018-08-28 23:15:02 +0530

    Changed appname to Reimagine kerala

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1 className="App-title">Rebuild Kerala</h1>
+      <h1 className="App-title">Reimagine Kerala</h1>
       </div>
     );
   }
