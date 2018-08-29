d174e5bb1491dc5a328dd94f4f5016f69c49e9a5
Aswin A V <aswinavofficial@gmail.com>
2018-08-29 14:19:49 +0530

    Fixed style issues

--- src/components/VolunteerListing.js
+++ src/components/VolunteerListing.js
@@ -25,17 +25,21 @@ const greenbgStyle = {
         <BreadcrumbItem><a style={whiteStyle} href="/">Home</a></BreadcrumbItem>
         <BreadcrumbItem active><a style={greenStyle} >Organizations willing to help</a></BreadcrumbItem>
       </Breadcrumb>
-        <Row>
-          <Col xs="3"></Col>
-          <Col xs="6">
-          {/* <h1 className="Vol-title" style={greenStyle} >Organizations willing to help</h1> */}
-          <Card body>
+        <Row >
+          <Col  md="6" >
+          <Card  className="card" body>
             <CardTitle className="Vol-title" style={greenStyle}>Free Carpentery work for org</CardTitle>
-            <CardText>Are you a public sector/social service institution (charitable hospitals, old age homes, backward community schools, etc) impacted by the floods? A Leading Manufacturing firm would like to help you get back on track by arranging free carpentry services for you</CardText>
+            <CardText className="cardtext">Are you a public sector/social service institution (charitable hospitals, old age homes, backward community schools, etc) impacted by the floods? A Leading Manufacturing firm would like to help you get back on track by arranging free carpentry services for you</CardText>
             <Button href="https://goo.gl/forms/hwosANvtyQ7tD3CR2" target="_blank" className="Vol-contactbutton" style={greenbgStyle}>Contact</Button>
           </Card>
           </Col>
-          <Col xs="3"></Col>
+          <Col  md="6" >
+          <Card className="card" body>
+            <CardTitle className="Vol-title" style={greenStyle}>Free Service - Appliances Repair</CardTitle>
+            <CardText className="cardtext">Information on the brands and dealerships offering free services to restore your appliances damaged in the disaster.<br/> <br/> <br/></CardText>
+            <Button href="tel:18002095511" target="_blank" className="Vol-contactbutton" style={greenbgStyle}>Visit</Button>
+          </Card>
+          </Col>
         </Row>
         </Container>
        
